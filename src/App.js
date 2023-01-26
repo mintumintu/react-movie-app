009fff10fd7e0ce09518760502595b5b0caf1453
Jogendra Pattnaik <jogendrapattnaik4@gmail.com>
2023-01-26 15:50:02 +0530

    Added alot of components but still buggy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
+import React, {useState,useEffect} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
+import AddFavourite from './components/AddFavourite';
 
 function App() {
+  const [movies,setMovies]= useState([]);
+  const [searchValue, setSearchValue] = useState('')
+  const [favourites,setFavourites] = useState([])
+
+  const getMovieRequest = async (searchValue)=>{
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=e2d4cd82`;
+    const response = await fetch(url);
+    const responseJson = await response.json();
+    if(responseJson.Search){
+      setMovies(responseJson.Search);
+    }
+  };
+  useEffect(()=>{
+    getMovieRequest(searchValue);
+  },[searchValue]);
+
+  const AddFavouriteMovie = (movie)=> {
+    const newFavouriteList = [...favourites,movie];
+    setFavourites(newFavouriteList);
+  }
   return (
-    <div className="App">
-  <h1>Hello</h1>
+    <div className="container-fluid movie-app">
+    <div className='row d-flex align-items-center mt-4 md-4'>
+      <MovieListHeading heading="Movies"/>
+      <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
+    </div>
+    <div className='row'>
+      <MovieList movies={movies}
+      handleFavouritesClick={AddFavouriteMovie}
+      favouriteComponent={AddFavourite}
+      />
+    </div>
+    {/* <div className='row d-flex align-items-center mt-4 mb-4'>
+      <MovieListHeading heading="Favourites" />
+    </div>    
+    <div className='row'>
+      <MovieList movies={favourites} />
+    </div> */}
     </div>
   );
 }
